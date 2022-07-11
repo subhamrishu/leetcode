2d74a33073fec93d584706aaceb87d3c7fb110c2
Subham Kumar <shub.rishu@gmail.com>
2022-07-11 14:21:55 +0530

    Time: 12 ms (44.85%), Space: 43.4 MB (85.00%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.java
+++ 463-island-perimeter/463-island-perimeter.java
@@ -1,5 +1,6 @@
 class Solution {
     int dir[] = {-1, 0, 1, 0, -1};
+    
     int bfs(int[][] grid, int i, int j){
         int r = grid.length, c = grid[0].length;
         Queue<int[]> q = new LinkedList();
@@ -9,8 +10,7 @@ class Solution {
             int[] curr = q.poll();
             int a = curr[0];
             int b = curr[1];
-             // System.out.println("Current: "+a+" "+b);
-            
+        
             if(grid[a][b] == -1)
                 continue;
             grid[a][b] = -1;//marking as visited
@@ -22,17 +22,11 @@ class Solution {
                 if(x >=0 && y>=0 && x <r && y < c && grid[x][y]==1){
                     q.offer(new int[]{x, y});
                     sum-=2;
-                    // System.out.println(x +" "+y+" "+sum);
                 }
             }
-            // System.out.println("\t" + Arrays.deepToString(grid) +" "+sum);
         }
         return sum;
-        // for (int i = 0; i < grid.length; i++){
-        //     for (int j = 0; j < grid[0].length; j++){
-        //         if 
-        //     }
-        // }
+
     }
     public int islandPerimeter(int[][] grid) {
         int r = grid.length, c = grid[0].length;
@@ -40,8 +34,8 @@ class Solution {
         for (int i = 0; i<r; i++){
             for(int j = 0; j<c;j++){
                 if (grid[i][j] == 1){
-                    perimeter = bfs(grid, i, j);
-                    break;
+                    return perimeter = bfs(grid, i, j);
+                    // break;
                 }
             }
         }
