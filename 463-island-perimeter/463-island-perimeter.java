3f39537e160a2e72762f57d2325b6778308cad2a
Subham Kumar <shub.rishu@gmail.com>
2022-07-11 14:31:24 +0530

    Time: 13 ms (36.69%), Space: 63 MB (23.03%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.java
+++ 463-island-perimeter/463-island-perimeter.java
@@ -34,7 +34,12 @@ class Solution {
         for (int i = 0; i<r; i++){
             for(int j = 0; j<c;j++){
                 if (grid[i][j] == 1){
-                    return perimeter = bfs(grid, i, j);
+                    perimeter+=4;
+                    if(j+1 < c && grid[i][j+1] == 1)
+                        perimeter -= 2;
+                    if(i+1 < r && grid[i+1][j] == 1)
+                        perimeter -= 2;
+                    // return perimeter = bfs(grid, i, j);
                     // break;
                 }
             }
