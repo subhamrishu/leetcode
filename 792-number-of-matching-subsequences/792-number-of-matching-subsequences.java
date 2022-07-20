676622fa8579c879ca36266e6324ed1c0be45a9f
Subham Kumar <shub.rishu@gmail.com>
2022-07-20 14:35:01 +0530

    Time: 53 ms (95.16%), Space: 44.1 MB (94.06%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
@@ -1,30 +1,44 @@
 
 class Solution {
+    class Node{
+        String word;
+        int index;
+        Node(){
+            
+        }
+        Node(String w, int i){
+            word =w;
+            index = i;
+        }
+    }
     public int numMatchingSubseq(String s, String[] words) {
-        // HashMap<Character, Integer> map = new HashMap();
-        // char[] c = s.toCharArray();
-        // // for (int i =0; i < c.length; i++){
-        // //     map.put(c, i);
-        // // }
-        // int count = 0;
-        // for (String word: words){
-        //     char w[] = word.toCharArray();
-        //     if (w.length > c.length)
-        //         continue;
-        //     int k, i, j = 0, c_len = c.length, local_count = 0;
-        //     for (i = 0; i < w.length && j < c_len; i++){
-        //         for (k = j; k < c_len; k++){
-        //             if (w[i] == c[k]){
-        //                 local_count += 1;
-        //                 break;
-        //             }
-        //         }
-        //         j = k+1;  
-        //     }
-        //     if (local_count == w.length)
-        //         count+=1;
+        /* Brute Force Solution: TLE
+        HashMap<Character, Integer> map = new HashMap();
+        char[] c = s.toCharArray();
+        // for (int i =0; i < c.length; i++){
+        //     map.put(c, i);
         // }
-        // return count;
+        int count = 0;
+        for (String word: words){
+            char w[] = word.toCharArray();
+            if (w.length > c.length)
+                continue;
+            int k, i, j = 0, c_len = c.length, local_count = 0;
+            for (i = 0; i < w.length && j < c_len; i++){
+                for (k = j; k < c_len; k++){
+                    if (w[i] == c[k]){
+                        local_count += 1;
+                        break;
+                    }
+                }
+                j = k+1;  
+            }
+            if (local_count == w.length)
+                count+=1;
+        }
+        return count;
+        */
+        /* Usng hashmap of lists : use binary search for optimisation
         Map<Character, ArrayList<Integer>> map = new HashMap();
         char[] c = s.toCharArray();
         for(int i = 0; i < c.length; i++){
@@ -55,6 +69,30 @@ class Solution {
             }
             if (localCount == w.length)
                 count ++;
+        }
+        return count;
+        */
+        // ArrayList<Node>[] mapping = new ArrayList[26];
+        HashMap<Character, Queue<Node>> map = new HashMap();
+        for (char ch = 'a'; ch <= 'z'; ch++){
+            map.put(ch, new LinkedList());
+        }
+        for (String word: words){
+            map.get(word.charAt(0)).add(new Node(word, 0));
+        }
+        int count = 0;
+        for(char ch: s.toCharArray()){
+            Queue<Node> currList = map.get(ch);
+            int size = currList.size();
+            for (int i =0; i< size; i++){
+                Node top = currList.poll();
+                top.index++;
+                if(top.index == top.word.length())
+                    count++;
+                else{
+                    map.get(top.word.charAt(top.index)).add(top);
+                }
+            }
         }
         return count;
     }
