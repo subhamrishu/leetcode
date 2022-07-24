7e4bb41191f92e0382eaf0b2c249f00137041c42
Subham Kumar <shub.rishu@gmail.com>
2022-07-24 23:16:16 +0530

    Time: 3 ms (90.85%), Space: 48.1 MB (14.51%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -10,6 +10,8 @@
  */
 class Solution {
     ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        if(l1 == null || l2 == null) 
+            return l1 != null ? l1 : l2;
         ListNode dummy = new ListNode();
         ListNode head = dummy;
         while (l1!=null && l2!=null){
@@ -31,12 +33,22 @@ class Solution {
         }
         return dummy.next;
     }
-    public ListNode mergeKLists(ListNode[] lists) {
+    ListNode mergeKListsHelper(ListNode[] lists, int left, int right) {
+        if(left > right) 
+            return null;
         
-        ListNode head = null;
-        for(int i = 0; i < lists.length; i++){
-            head = mergeTwoLists(head, lists[i]);
-        }
-        return head;
+        if(left == right) 
+            return lists[left];
+        
+        int mid = (left + right) / 2;
+        ListNode l1 = mergeKListsHelper(lists, left, mid);
+        ListNode l2 = mergeKListsHelper(lists, mid + 1, right);
+        
+        return mergeTwoLists(l1, l2);
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0)
+            return null;
+        return mergeKListsHelper(lists, 0, lists.length-1);
     }
 }
