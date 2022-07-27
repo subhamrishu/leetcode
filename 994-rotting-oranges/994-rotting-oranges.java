6a1fc479a8f4cc7bb1f980cff7219cb79be0dfc9
Subham Kumar <shub.rishu@gmail.com>
2022-07-27 16:52:32 +0530

    Time: 2 ms (96.16%), Space: 42.5 MB (77.05%) - LeetHub

---
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -0,0 +1,44 @@
+class Solution {
+//     class Cell{
+//         int x, y;
+//         Cell(int x, int y){
+            
+//         }
+//     }
+    int dir[] = {-1, 0, 1, 0, -1};
+
+    void helper(int[][] grid, int x, int y, int val){
+        int r = grid.length, c = grid[0].length;
+        
+        if (grid[x][y] == 1) 
+            grid[x][y] = val+1;
+        else 
+            grid[x][y] = Math.min(val+1, grid[x][y]);
+        for (int i = 0; i < 4; i++){
+            int xx = x + dir[i];
+            int yy = y + dir[i+1];
+            if (xx >=0 && xx < r && yy >= 0 && yy < c && grid[xx][yy]!=0 && grid[xx][yy]!=2 && (grid[xx][yy] == 1 || grid[xx][yy] > grid[x][y]+1)){
+                helper(grid, xx, yy, grid[x][y]);
+            }
+        }
+        // max = Math.max(max, grid[x][y]);
+    }
+    public int orangesRotting(int[][] grid) {
+        
+        for (int i = 0; i<grid.length; i++){
+            for (int j = 0; j< grid[0].length; j++){
+                if (grid[i][j] == 2)
+                    helper(grid, i, j, 2);
+            }
+        }
+        int max = 0;
+        for (int i = 0; i<grid.length; i++){
+            for (int j = 0; j< grid[0].length; j++){
+                if (grid[i][j] == 1)
+                    return -1;
+                max = Math.max(max, grid[i][j]);
+            }
+        }
+        return max == 0? 0 : max-2;
+    }
+}
