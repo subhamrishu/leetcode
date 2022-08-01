dea0dfc91d96949e07aa6e29f05498659cd2406c
Subham Kumar <shub.rishu@gmail.com>
2022-08-01 10:54:06 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (13.36%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,5 +1,6 @@
 class Solution {
     // int sol = 0;
+    /* Recursive with memo solution - 0ms
     Integer[][] dp;
     int dfs(int i, int j, int m, int n){
         if (i == m && j == n){
@@ -11,8 +12,21 @@ class Solution {
             return dp[i][j];
         return dp[i][j] = dfs(i+1, j, m, n) + dfs(i, j+1, m, n);
     }
+    */
     public int uniquePaths(int m, int n) {
-        dp = new Integer[m][n];
-        return dfs(0, 0, m-1, n-1);
+        // dp = new Integer[m][n];
+        // return dfs(0, 0, m-1, n-1);
+        int[] first = new int[n];
+        int[] second = new int[n];
+        second[0]=1;
+        for (int i = 0; i < m; i++){
+            for (int j = 1; j<n; j++){
+                second[j] = first[j] + second[j-1]; 
+            }
+            first = second;
+            second = new int[n];
+            second[0] = 1;
+        }
+        return first[n-1];
     }
 }
