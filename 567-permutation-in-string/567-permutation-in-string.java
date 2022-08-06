55a2d7450119ba6dd7cd7465b1b2ccf68004ee02
Subham Kumar <shub.rishu@gmail.com>
2022-08-06 23:42:01 +0530

    Time: 5 ms (93.93%), Space: 43 MB (71.37%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,23 +1,30 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int[] s1Hash = new int[26];
-        for (char c: s1.toCharArray()){
-            s1Hash[c-'a']++;
-        }
-        int l1 = s1.length();
-        char[] s2ch = s2.toCharArray();
+        int len1 = s1.length(), len2 = s2.length();
+        if (len1 > len2) return false;
         
-        for (int i = 0; i <= s2ch.length-l1; i++){
-            int j = i, count = 0;
-            int[] copy=s1Hash.clone();
-            while(j < s2.length() && copy[s2ch[j]-'a'] > 0){
-                copy[s2ch[j]-'a']--;
-                count++;
-                j++;
-            }
-            if (count == l1)
-                return true;
+        int[] count = new int[26];
+        char[] str1 = s1.toCharArray();
+        char[] str2 = s2.toCharArray();
+        for (int i = 0; i < len1; i++) {
+            count[str1[i] - 'a']++;
+            count[str2[i] - 'a']--;
         }
+        if (allZero(count)) return true;
+        
+        for (int i = len1; i < len2; i++) {
+            count[str2[i]- 'a']--;
+            count[str2[i-len1] - 'a']++;
+            if (allZero(count)) return true;
+        }
+        
         return false;
+    }
+    
+    boolean allZero(int[] count) {
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
+        }
+        return true;
     }
 }
