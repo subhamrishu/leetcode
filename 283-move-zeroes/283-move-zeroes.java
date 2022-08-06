eb13deac13c148cee7ecf18b69323351f357f12c
Subham Kumar <shub.rishu@gmail.com>
2022-08-06 11:41:00 +0530

    Time: 2 ms (82.89%), Space: 54.6 MB (67.41%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,14 +1,10 @@
 class Solution {
-    void swap(int[] nums, int i , int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
+
     public void moveZeroes(int[] nums) {
         int zeroCount = 0, i = 0, j = 0;
         while (j < nums.length){
             if (nums[j] != 0){
-                swap(nums, i, j);
+                nums[i] = nums[j];
                 i++;
             }
             else{
@@ -16,12 +12,9 @@ class Solution {
             }
             j++;
         }
-        // System.out.println(Arrays.toString(nums));
-        
         while(zeroCount > 0){
             nums[i++] = 0;
             zeroCount--;
-        }
-        // System.out.println(Arrays.toString(nums));
+        } 
     }
 }
