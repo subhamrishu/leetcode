cffbd29256b89dd62b16cb914b380d9e6062760e
Subham Kumar <shub.rishu@gmail.com>
2022-08-07 11:40:33 +0530

    Time: 3 ms (49.86%), Space: 47.8 MB (26.47%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
@@ -22,18 +22,32 @@ class Node {
 */
 
 class Solution {
-    void helper(Node root1, Node root2){
-        if (root1 == null)
-            return; 
-        root1.next = root2;
-        helper(root1.left, root1.right);
-        helper(root2.left, root2.right);
-        helper(root1.right, root2.left);
-    }
+    // void helper(Node root1, Node root2){
+    //     if (root1 == null)
+    //         return; 
+    //     root1.next = root2;
+    //     helper(root1.left, root1.right);
+    //     helper(root2.left, root2.right);
+    //     helper(root1.right, root2.left);
+    // }
     public Node connect(Node root) {
         if (root == null)
             return null;
-        helper(root.left, root.right);
+        // helper(root.left, root.right);
+        Queue<Node> q = new LinkedList();
+        q.offer(root);
+        while (q.size()!=0){
+            Node rightNode = null;
+            for (int i = q.size(); i >0; i--){
+                Node curr = q.poll();
+                curr.next = rightNode;
+                rightNode = curr;
+                if (curr.left!=null){
+                    q.offer(curr.right);
+                    q.offer(curr.left);
+                }
+            }
+        }
         return root;
     }
 }
