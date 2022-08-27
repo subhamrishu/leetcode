a05b85cc41844d7f36448c553124f5e7504c3b8b
Subham Kumar <shub.rishu@gmail.com>
2022-08-27 20:41:51 +0530

    Time: 11 ms (95.96%), Space: 134.7 MB (7.65%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.java
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -3,6 +3,7 @@ class Solution {
         int n = temperatures.length;
         int[] sol = new int[n];
         
+        /* ===========using Stack===========
         Stack<Integer> stack = new Stack<>();
         
         for (int i = 0; i < n; i++){
@@ -11,6 +12,18 @@ class Solution {
                 sol[smallerIndex] = i - smallerIndex;
             }
             stack.push(i);
+        }
+        return sol;
+        */
+        /*============using Array===========*/
+        int[] stack = new int[n];
+        int top = -1;
+        for (int i = 0; i < n; i++){
+            while (top > -1 && temperatures[i] > temperatures[stack[top]]){
+                int smallerIndex = stack[top--];
+                sol[smallerIndex] = i - smallerIndex;
+            }
+            stack[++top] = i;
         }
         return sol;
     }
