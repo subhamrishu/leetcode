04b6ac5f7c7f104cf29065f0554b01cdca96c8b7
Subham Kumar <shub.rishu@gmail.com>
2022-10-21 02:00:53 +0530

    Time: 16 ms (88.62%), Space: 54.1 MB (43.71%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -5,11 +5,18 @@ class Solution {
             if (n%i == 0){
                 int times = n/i;
                 String pattern = s.substring(0, i);
-                StringBuilder sb = new StringBuilder();
-                for (int j = 0; j<times; j++){
-                    sb.append(pattern);
+                // StringBuilder sb = new StringBuilder();
+                // for (int j = 0; j<times; j++){
+                //     sb.append(pattern);
+                // }
+                // if (sb.toString().equals(s))
+                //     return true;
+                int j;
+                for (j = 1; j<times;j++){
+                    if (!pattern.equals(s.substring(j*i, i+j*i)))
+                        break;
                 }
-                if (sb.toString().equals(s))
+                if (j == times)
                     return true;
             }
         }
