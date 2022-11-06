3ff82065bf7781bd94f89d61281f9a0d3a2b6bcd
Subham Kumar <shub.rishu@gmail.com>
2022-11-06 11:23:23 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (95.15%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,18 +1,39 @@
 class Solution {
-    public int strStr(String haystack, String needle) {
-        int ln = needle.length(), lh = haystack.length();
-        if(lh < ln)
-            return -1;
-        int n = 0, h = 0;
-        while (n < ln && h < lh){
-            while (h < lh && n < ln && haystack.charAt(h) == needle.charAt(n)){
-                h++; n++;
+    int[] buildLPS(String needle){
+        int n = needle.length();
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+        int i = 0, j =1;
+        while (j < n){
+            if (needle.charAt(i) == needle.charAt(j)){
+                res[j++] = i++; 
             }
-            if (n == ln)
-                return h - n;
-            h = (n == 0) ? h+1 : h-n+1;
-            n = 0;
-            
+            else if (i > 0){
+                i = res[i-1] + 1;
+            }
+            else
+                j += 1;
+        }
+        return res;
+    }
+    public int strStr(String haystack, String needle) {
+        if (needle.length() > haystack.length())
+            return -1;
+        int[] lps = buildLPS(needle);
+        int m = haystack.length(), n = needle.length();
+        int i = 0, j = 0;
+        while (i < m){
+            if (haystack.charAt(i) == needle.charAt(j)){
+                if (j == n-1){
+                    return i - n + 1;
+                }
+                i += 1;
+                j += 1;
+            }
+            else if (j > 0)
+                j = lps[j-1] + 1;
+            else
+                i += 1;
         }
         return -1;
     }
