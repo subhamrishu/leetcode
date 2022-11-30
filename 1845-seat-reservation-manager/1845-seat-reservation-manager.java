8e4793daa04b49aae1f7f5136e548898b345d93a
Subham Kumar <shub.rishu@gmail.com>
2022-11-30 19:58:46 +0530

    Time: 79 ms (87.29%), Space: 103.9 MB (73.46%) - LeetHub

--- 1845-seat-reservation-manager/1845-seat-reservation-manager.java
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.java
@@ -1,13 +1,18 @@
 class SeatManager {
     PriorityQueue<Integer> pq;
+    int seatNumSmall;
     public SeatManager(int n) {
-        pq = new PriorityQueue<>(n+1);
-        for (int i = 1; i <= n; i++){
-            pq.add(i);
-        }
+        pq = new PriorityQueue<>(n);
+        seatNumSmall = 0;
+        // for (int i = 1; i <= n; i++){
+        //     pq.add(i);
+        // }
     }
     
     public int reserve() {
+        if (pq.size() == 0){
+            return ++seatNumSmall;
+        }
         return pq.poll();
     }
     
