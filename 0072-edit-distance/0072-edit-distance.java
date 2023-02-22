e701bd7ca96a1f4869b9e59f8f82b5b1ad9cf1fa
Subham Kumar <shub.rishu@gmail.com>
2023-02-22 12:38:40 -0800

    Time: 5 ms (75.91%), Space: 46.2 MB (5.31%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -5,27 +5,29 @@ class Solution {
             return word2.length() - p2;
         else if (p2 == word2.length())
             return word1.length() - p1;
+        
         if (dp[p1][p2] != -1)
             return dp[p1][p2];
-        if (word1.charAt(p1) == word2.charAt(p2))
-            return helper(word1, word2, p1+1, p2+1);
         int min = Integer.MAX_VALUE;
-        //insert
-        min = Math.min(min, helper(word1, word2, p1, p2+1));
-        //delete
-        min = Math.min(min, helper(word1, word2, p1+1, p2));
-        min = Math.min(min, helper(word1, word2, p1+1, p2+1));
-        
-        return dp[p1][p2] = min + 1;
+        //characters match, move both the pointers
+        if (word1.charAt(p1) == word2.charAt(p2))
+            dp[p1][p2] = helper(word1, word2, p1+1, p2+1);
+        else{
+            //insert
+            min = Math.min(min, helper(word1, word2, p1, p2+1));
+            //delete
+            min = Math.min(min, helper(word1, word2, p1+1, p2));
+            min = Math.min(min, helper(word1, word2, p1+1, p2+1));
+            dp[p1][p2] = min + 1;
+        }
+        return dp[p1][p2];
         
     }
     public int minDistance(String word1, String word2) {
         dp = new int[word1.length()+1][word2.length()+1];
-        for (int i = 0; i < word1.length(); i++){
-            for (int j = 0; j < word2.length(); j++){
-                dp[i][j] = -1;
+        for (int row[] : dp){
+                Arrays.fill(row, -1);
             }
-        }
         return helper(word1, word2, 0, 0);
     }
 }
