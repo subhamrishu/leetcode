ff91f41aecf06db65ae284eca7bfb8a67a9d6c1b
Subham Kumar <shub.rishu@gmail.com>
2023-06-03 12:58:05 +0530

    Time: 127 ms (28.11%), Space: 61.4 MB (49.72%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,21 +1,46 @@
 class Solution {
-    int dfs(List<Integer>[] graph, int[] informTime, int u){
-        int timeOfNode = 0;
-        for (int v: graph[u]){
-            timeOfNode = Math.max(timeOfNode, dfs(graph, informTime, v));
+    private int dfs(int head, Map<Integer, ArrayList> graph, int[] informTime){
+        if (informTime[head] == 0)
+            return 0;
+        int max = 0;
+        ArrayList<Integer> curr = graph.get(head);
+        for (int emp: curr){
+            max = Math.max(max, dfs(emp, graph, informTime));
         }
-        return timeOfNode+informTime[u];
+        return max + informTime[head];
     }
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        List<Integer>[] graph = new List[n];
-        for (int i = 0; i < n; i++){
-            graph[i] = new ArrayList<>();
-        }
-        for (int i = 0; i < n; i++){
+        Map<Integer, ArrayList> graph = new HashMap<>();
+        for (int i = 0; i < manager.length; i++){
             if(i != headID){
-                graph[manager[i]].add(i);
+                graph.putIfAbsent(manager[i], new ArrayList<Integer>());
+                graph.get(manager[i]).add(i);
             }
         }
-        return dfs(graph, informTime, headID);
+        return dfs(headID, graph, informTime);
+//         Queue<Integer> q = new LinkedList<>();
+//         if (graph.containsKey(headID))
+//             q.add(headID);
+//         int totalTime = 0;
+//         // totalTime += informTime[headID];
+//         while(!q.isEmpty()){
+            
+//             int currSize = q.size();
+//             int max = 0;
+//             for (int i = 0; i < currSize; i++){
+//                 int currEmp = q.poll();
+//                 max = Math.max(max, informTime[currEmp]);
+//                 ArrayList<Integer> curr = graph.get(currEmp);
+//                 for (int emp: curr){
+//                     if(informTime[emp] != 0){
+//                         // totalTime += informTime[emp];
+//                         q.add(emp);
+//                     }
+//                 }
+//             }
+//             totalTime += max;
+//         }
+//         return totalTime;
+        // return 0;
     }
 }
