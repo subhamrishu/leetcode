55b2e8a01427a3487a2fbb1f81faace60958f789
Subham Kumar <shub.rishu@gmail.com>
2023-06-05 04:19:42 +0530

    Time: 1 ms (98.95%), Space: 44.3 MB (15.20%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,33 +1,34 @@
 class Solution {
-    private void dfs(int i, boolean[] visited, List<Integer>[] graph){
+    private void dfs(int i, boolean[] visited, int[][] isConnected){
         visited[i] = true;
-        for (int k : graph[i]){
-            if (!visited[k])
-                dfs(k, visited, graph);
+        for (int k = 0; k < isConnected.length; k++){
+            if (isConnected[i][k]==1 && !visited[k])
+                dfs(k, visited, isConnected);
         }
     }
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        List<Integer>[] graph = new List[n];
-        for (int i = 0; i< n; i++){
-            graph[i] = new ArrayList<>();
-        }
-        for (int i = 0; i<n ;i++){
-            for (int j = i+1; j < n; j++){
-                if (isConnected[i][j] == 1){
-                    graph[i].add(j);
-                    graph[j].add(i);
-                }
-            }
-        }
+        // List<Integer>[] graph = new List[n];
+        // for (int i = 0; i< n; i++){
+        //     graph[i] = new ArrayList<>();
+        // }
+        // for (int i = 0; i<n ;i++){
+        //     for (int j = i+1; j < n; j++){
+        //         if (isConnected[i][j] == 1){
+        //             graph[i].add(j);
+        //             graph[j].add(i);
+        //         }
+        //     }
+        // }
         int provinces = 0;
         boolean[] visited = new boolean[n];
-        Arrays.fill(visited, false);
+        // Arrays.fill(visited, false);
         for(int i = 0; i < n; i++){
             // System.out.println(graph[i]);
-            if (!visited[i])
+            if (!visited[i]){
                 provinces++;
-            dfs(i, visited, graph);
+                dfs(i, visited, isConnected);
+            }
             // System.out.println(i+" "+provinces+ " "+Arrays.toString(visited));
         }
         return provinces;
