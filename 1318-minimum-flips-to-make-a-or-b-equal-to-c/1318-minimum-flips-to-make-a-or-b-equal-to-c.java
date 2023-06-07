7192a71d1ffc6184e88f9e131c16fb92a4c57f64
Subham Kumar <shub.rishu@gmail.com>
2023-06-07 19:41:41 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (88.89%) - LeetHub

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -7,26 +7,16 @@ class Solution {
                    count++; 
                 }
             }
-            else if ((c&1) == 0){
+            else {
                 if ((a&1) == 1)
                     count++;
                 if ((b&1) == 1)
                     count++;
             }
-            c = c >> 1;
-            b = b >> 1;
-            a = a >> 1;
+            c >>= 1;
+            b >>= 1;
+            a >>= 1;
         }
-        // System.out.println("a="+a+" b="+b+" c="+c+" count="+count);
-        // while (a!=0 || b!=0){
-        //     if ((a&1) == 1)
-        //             count++;
-        //     if ((b&1) == 1)
-        //         count++;
-        //     a = a >> 1;
-        //     b = b >> 1;
-        // }
-        // System.out.println("a="+a+" b="+b+" c="+c+" count="+count);
         return count;
     }
 }
