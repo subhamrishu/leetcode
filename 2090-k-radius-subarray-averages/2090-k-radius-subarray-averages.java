b3c10b6523977369daa85ee2b25a6c9f76d1e13f
Subham Kumar <shub.rishu@gmail.com>
2023-06-20 06:17:12 +0530

    Time: 14 ms (8.01%), Space: 64.1 MB (7.80%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -3,11 +3,14 @@ class Solution {
         int n = nums.length;
         long[] prefixSum = new long[n+1];
         long div = 2*k+1;
-        
+        int[] sol = new int[n];
+        Arrays.fill(sol, -1);
+        if (div > n)
+            return sol;
         for (int i = 0; i < n; i++){
             prefixSum[i+1] += prefixSum[i] + nums[i];
         }
-        int[] sol = new int[n];
+        
         for (int i = 0; i < n; i++){
             if (i+k >= n || i-k < 0){
                 sol[i] = -1;
