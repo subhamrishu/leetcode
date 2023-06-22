f8c46eab410b9f7db0ec9535c6da6b5525e5b9b9
Subham Kumar <shub.rishu@gmail.com>
2023-06-23 02:31:40 +0530

    Time: 20 ms (41.86%), Space: 56.3 MB (26.76%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,18 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-        int n = prices.length;
-        int[][] dp = new int[n+1][2];
-        //0 denotes not bought yet- can buy
-        //1 denotes bought can be sold now
-        dp[0][0] = -prices[0]-fee;
-        for (int i = 1; i < n; i++){
-            for (int j = 0; j < 2; j++){
-                if (j == 0) //buy stocks
-                    dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] - prices[i] - fee);
-                else //sell stocks
-                    dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] + prices[i]);
-            }
+         int n = prices.length;
+        int[][] stocks = new int[n][2];
+        //stocks[i][0] denotes sell, stocks[i][1] denotes buy 
+        stocks[0][0] = 0; 
+        stocks[0][1] = -prices[0]-fee;
+        for (int i = 1; i < prices.length; i++){
+            stocks[i][0] = Math.max(stocks[i-1][0], stocks[i-1][1] + prices[i]);
+            stocks[i][1] = Math.max(stocks[i-1][1], stocks[i-1][0] - prices[i] - fee);
         }
-        return dp[n-1][1];
+        return stocks[n-1][0];
     }
 }
