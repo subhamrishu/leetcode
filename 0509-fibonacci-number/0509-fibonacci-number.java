37393ed71125510077480f1ece04f8be164229b6
Subham Kumar <shub.rishu@gmail.com>
2023-06-26 06:33:00 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (65.83%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    int[] memo = new int[31];
+    public int fib(int n) {
+        if (n <= 1)
+            return n;
+        if (memo[n] != 0)
+            return memo[n];
+        return memo[n] = fib(n-1) + fib(n-2);
+    }
+}
