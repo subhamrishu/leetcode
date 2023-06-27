0fd21f04a52c1a8d3f8b7a8d5878b9465cca2a15
Subham Kumar <shub.rishu@gmail.com>
2023-06-28 05:22:23 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (60.84%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -6,13 +6,15 @@ class Solution {
         else if (i == grid.length-1 && j == grid[0].length-1)
             return 1;
         
-        else if (memo[i][j] != 0)
+        else if (memo[i][j] != -1)
             return memo[i][j];
         return memo[i][j] = helper(i+1, j, grid) + helper(i, j+1, grid);
     }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length, n = obstacleGrid[0].length;
         memo = new int[m][n];
+        for (int i = 0; i < m; i++)
+            Arrays.fill(memo[i], -1);
         return helper(0,0,obstacleGrid);
     }
 }
