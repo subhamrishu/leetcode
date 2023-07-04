70ac5576c8d3f3bb002d4a3ea3971048d3c733d2
Subham Kumar <shub.rishu@gmail.com>
2023-07-04 11:52:22 +0530

    Time: 7 ms (36.45%), Space: 43.7 MB (62.23%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,33 +1,27 @@
 class Solution {
-    int[][] dp;
-    int helper(String word1, String word2, int p1, int p2){
-        if (p1 == word1.length())
-            return word2.length() - p2;
-        else if (p2 == word2.length())
-            return word1.length() - p1;
-        
-        if (dp[p1][p2] != -1)
-            return dp[p1][p2];
-        int min = Integer.MAX_VALUE;
-        //characters match, move both the pointers
-        if (word1.charAt(p1) == word2.charAt(p2))
-            dp[p1][p2] = helper(word1, word2, p1+1, p2+1);
-        else{
-            //insert
-            min = Math.min(min, helper(word1, word2, p1, p2+1));
-            //delete
-            min = Math.min(min, helper(word1, word2, p1+1, p2));
-            min = Math.min(min, helper(word1, word2, p1+1, p2+1));
-            dp[p1][p2] = min + 1;
-        }
-        return dp[p1][p2];
-        
+    int memo[][];
+    int helper(int i, int j, String w1, String w2){
+        if (i == w1.length() && j == w2.length())
+            return 0;
+        else if (i == w1.length())
+            return w2.length() - j;
+        else if (j == w2.length())
+            return w1.length() - i;
+        if (memo[i][j] != -1)
+            return memo[i][j];
+        else if (w1.charAt(i) == w2.charAt(j))
+            return helper(i+1, j+1, w1, w2);
+        int insert = helper(i, j+1, w1, w2);
+        int delete = helper(i+1, j, w1, w2);
+        int replace = helper(i+1, j+1, w1, w2);
+        return memo[i][j] = 1 + Math.min(insert, Math.min(delete, replace));
     }
     public int minDistance(String word1, String word2) {
-        dp = new int[word1.length()+1][word2.length()+1];
-        for (int row[] : dp){
-                Arrays.fill(row, -1);
-            }
-        return helper(word1, word2, 0, 0);
+        int n = word1.length(), m = word2.length();
+        memo = new int[n][m];
+        for (int[] arr: memo){
+            Arrays.fill(arr, -1);
+        }
+        return helper(0, 0, word1, word2);
     }
 }
