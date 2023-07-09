dfe9ef6b6a4e70cbe91c04e0401aefc3ac91ab87
Subham Kumar <shub.rishu@gmail.com>
2023-07-09 11:58:57 +0530

    Time: 1 ms (99.98%), Space: 44.9 MB (12.74%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,24 +1,30 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
-        boolean[] canJump = new boolean[n];
-        canJump[n-1] = true;
+        // boolean[] canJump = new boolean[n];
+        // canJump[n-1] = true;
+        // for (int i = n-2; i>=0; i--){
+        //     if (nums[i] != 0){
+        //         int maxJump = i + nums[i];
+        //         if (maxJump >= n-1){
+        //             canJump[i] = true;
+        //             continue;
+        //         }
+        //         while (maxJump > i){
+        //             if (canJump[maxJump] == true){
+        //                 canJump[i] = true;
+        //                 break;
+        //             }
+        //             maxJump--;
+        //         }
+        //     }
+        // }
+        // return canJump[0];
+        int target = n-1;
         for (int i = n-2; i>=0; i--){
-            if (nums[i] != 0){
-                int maxJump = i + nums[i];
-                if (maxJump >= n-1){
-                    canJump[i] = true;
-                    continue;
-                }
-                while (maxJump > i){
-                    if (canJump[maxJump] == true){
-                        canJump[i] = true;
-                        break;
-                    }
-                    maxJump--;
-                }
-            }
+            if ((nums[i] + i) >= target)
+                target = i;
         }
-        return canJump[0];
+        return target == 0;
     }
 }
