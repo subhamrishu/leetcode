3800a3f0863b1c4ab9c18e32af350d4939bcd77e
Subham Kumar <shub.rishu@gmail.com>
2023-07-17 11:57:15 +0530

    Time: 371 ms (14.07%), Space: 40.9 MB (29.24%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,50 @@
+class Solution {
+    int[] dir = {-1, 0, 1, 0, -1};
+    boolean helper(char[][] board, String word, StringBuilder sb, int i, int j, boolean[][] visited){
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j])
+            return false;
+        
+        // System.out.println(word.charAt(sb.length())+ " " + sb.toString() + " "+ board[i][j]);
+        if (word.charAt(sb.length()) != board[i][j])
+            return false;
+        
+        
+        // if (sb.length() == word.length())
+        //     System.out.println(sb.toString());
+//         if (sb.toString().equals(word))
+//             return true;
+//         else if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j]){
+//             return false;
+//         }
+//         else if (sb.length() >= word.length())
+//             return false;
+        
+        visited[i][j] = true;
+        sb.append(board[i][j]);
+        if (sb.length() == word.length()){
+            if (sb.toString().equals(word))
+                return true;
+            else 
+                return false;
+        }
+        // System.out.println(sb.toString());
+        for (int k = 0; k < 4; k++){
+            int i1 = i + dir[k], j1 = j + dir[k+1];
+            if (helper(board, word, sb, i1, j1, visited)) return true;
+        }
+        sb.deleteCharAt(sb.length()-1);
+        visited[i][j] = false;
+        return false;
+    }
+    public boolean exist(char[][] board, String word) {
+        int m = board.length, n = board[0].length;
+        if (m == 1 && n == 1 && word.length() == 1)
+            return board[0][0] == word.charAt(0);
+        for (int i = 0; i < m ; i++){
+            for (int j = 0; j < n; j++){
+                if (helper(board, word, new StringBuilder(), i, j, new boolean[m][n])) return true;
+            }
+        }
+        return false;
+    }
+}
