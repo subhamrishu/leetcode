33749b1d7c8e229050bb820305032f3e545888e8
Subham Kumar <shub.rishu@gmail.com>
2023-07-23 08:04:59 +0530

    Time: 2 ms (88.88%), Space: 41.1 MB (87.91%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,50 +1,36 @@
 class Solution {
-    int max;
-    void dfs(int[][] grid, int[][] time, boolean[][] visited, int i, int j, int currTime){
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || visited[i][j])
-            return;
-        visited[i][j] = true;
-        
-        time[i][j] = Math.min(time[i][j], currTime+1);
-        // max = Math.max(max, time[i][j]);
-        // System.out.println(time[i][j]+ " "+i + " "+j);
-        if (i+1 < grid.length && grid[i+1][j] == 1)
-            dfs(grid, time, visited, i+1, j, currTime+1);
-        if (i-1 >= 0 && grid[i-1][j] == 1)
-            dfs(grid, time, visited, i-1, j, currTime+1);
-        if (j+1 < grid[0].length && grid[i][j+1] == 1)
-            dfs(grid, time, visited, i, j+1, currTime+1);
-        if (j-1 >= 0 && grid[i][j-1] == 1)
-            dfs(grid, time, visited, i, j-1, currTime+1);
-        visited[i][j] = false;
-        
-    }
     public int orangesRotting(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        int[][] time = new int[m][n];
-        max = Integer.MIN_VALUE;
-        for (int[] row: time){
-            Arrays.fill(row, 101);
+        Queue<int[]> rotten = new LinkedList<>();
+        int freshCount = 0;
+        int m = grid.length, n= grid[0].length;
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++){
+                if (grid[i][j] == 2)
+                    rotten.add(new int[]{i, j});
+                else if (grid[i][j] == 1)
+                    freshCount++;
+            }
         }
-        boolean[][] visited = new boolean[m][n];
-        for (int i= 0; i< m; i++){
-            for (int j = 0; j < n ; j++){
-                if (grid[i][j] == 2){
-                    visited = new boolean[m][n];
-                    dfs(grid, time, visited, i, j, -1);
-                    // System.out.println(Arrays.deepToString(time));
+        if (freshCount == 0) return 0;
+        int[] dir = new int[]{-1, 0, 1, 0, -1};
+        int count = 0;
+        while (!rotten.isEmpty()){
+            count++;
+            for (int k = rotten.size(); k > 0; k--){
+                int[] curr = rotten.poll();
+                for (int i = 0; i < 4; i++){
+                    int x = curr[0] + dir[i];
+                    int y = curr[1] + dir[i+1];
+
+                    if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] == 0 || grid[x][y] == 2)
+                        continue;
+                    grid[x][y] = 2;
+                    rotten.add(new int[]{x, y});
+                    freshCount--;
                 }
             }
         }
-        for (int i= 0; i< m; i++){
-            for (int j = 0; j < n ; j++){
-                if (grid[i][j] == 1){
-                    max = Math.max(max, time[i][j]);
-                }
-            }
-        }
-        if (max == 101)
-            max = -1;
-        return max == Integer.MIN_VALUE? 0 : max;
+        // System.out.println(Arrays.deepToString(grid));
+        return freshCount == 0? count - 1: -1;
     }
 }
