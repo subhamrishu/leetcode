5f2733b8ff4a86d632bb9c664555779f30e92b45
Subham Kumar <shub.rishu@gmail.com>
2023-09-08 14:09:03 -0700

    Time: 32 ms (54.20%), Space: 48.9 MB (28.24%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,68 +1,34 @@
 class Solution {
-    int count = 0;
-    class TrieNode{
-        TrieNode[] children;
-        boolean endFlag;
-        String word;
-        TrieNode(){
-            children = new TrieNode[26];
-            endFlag = false;
-            word = "";
-        }
-    }
-    class Trie{
-        TrieNode root;
-        Trie(){
-            root = new TrieNode();
-        }
-        void addWord(String word){
-            TrieNode curr = root;
-            for (char ch: word.toCharArray()){
-                int idx = ch - 'a';
-                if(curr.children[idx] == null)
-                    curr.children[idx] = new TrieNode();
-                curr = curr.children[idx];
-            }
-            curr.endFlag = true;
-            curr.word = word;
-        }
-        void suggest(TrieNode curr, List<String> sol){
-            
-            if (curr.endFlag){
-                sol.add(curr.word);
-                count++;
-            }
-            for (int j = 0; j < 26 && count < 3; j++ ){
-                if (curr.children[j] != null)
-                    suggest(curr.children[j], sol);
-            } 
-            
-        }
-        List<String> search(String prefix){
-            List<String> sol = new ArrayList<>();
-            TrieNode curr = root;
-            for (char ch: prefix.toCharArray()){
-                if (curr.children[ch-'a'] == null)
-                    return sol;
-                curr = curr.children[ch-'a'];
-            }
-            
-            suggest(curr, sol);
-            return sol;
-            
-        }
+    class Trie {
+        Trie[] sub = new Trie[26];
+        LinkedList<String> suggestion = new LinkedList<>();
     }
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        List<List<String>> sol = new ArrayList<>();
-        Trie trie = new Trie();
-        for (String product: products){
-            trie.addWord(product);
+        Trie root = new Trie();
+        for (String p : products) { // build Trie.
+            insert(p, root); // insert a product into Trie.
         }
-        StringBuilder sb = new StringBuilder();
-        for (char ch: searchWord.toCharArray()){
-            count = 0;
-            sol.add(trie.search(sb.append(ch).toString()));
+        return search(searchWord, root);
+    }
+    private void insert(String p, Trie root) {
+        Trie t = root;
+        for (char c : p.toCharArray()) { // insert current product into Trie.
+            if (t.sub[c - 'a'] == null)
+                t.sub[c - 'a'] = new Trie();
+            t = t.sub[c - 'a'];
+            t.suggestion.offer(p); // put products with same prefix into suggestion list.
+            Collections.sort(t.suggestion);
+            if (t.suggestion.size() > 3) // maintain 3 lexicographically minimum strings.
+                t.suggestion.pollLast();        
         }
-        return sol;
+    }
+    private List<List<String>> search(String searchWord, Trie root) {
+        List<List<String>> ans = new ArrayList<>();
+        for (char c : searchWord.toCharArray()) { // search product.
+            if (root != null) // if there exist products with current prefix.
+                root = root.sub[c - 'a'];
+            ans.add(root == null ? Arrays.asList() : root.suggestion); // add it if there exist products with current prefix.
+        }
+        return ans;
     }
 }
