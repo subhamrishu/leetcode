3c51f64864a4f7c2931602cebdd82db478bf28ba
Subham Kumar <shub.rishu@gmail.com>
2023-11-07 11:48:56 -0800

    Time: 1 ms (99.95%), Space: 43.7 MB (59.71%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -7,9 +7,12 @@ class Solution {
             return;
         }
         for (int i = curr; i < candidates.length; i++){
-            temp.add(candidates[i]);
-            dfs(candidates, target-candidates[i], i, temp, sol);
-            temp.remove(temp.size()-1);
+            if (target >= candidates[i]){
+                temp.add(candidates[i]);
+                dfs(candidates, target-candidates[i], i, temp, sol);
+                temp.remove(temp.size()-1);
+            }
+            
         }
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
