1620a29f02e358e9cd5652ff86c73bad300f127d
Subham Kumar <shub.rishu@gmail.com>
2023-12-01 13:22:25 -0800

    Time: 0 ms (100.00%), Space: 40.2 MB (65.53%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -1,18 +1,26 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
         for (int i = 0; i < word1.length; i++){
-            sb.append(word1[i]);
+            sb1.append(word1[i]);
         }
-        int k = 0, i = 0, j = 0;
-        for (i = 0; i < word2.length; i++){
-            for (j = 0; j < word2[i].length() && k < sb.length(); j++){
-                if (sb.charAt(k) != word2[i].charAt(j))
-                    return false;
-                k++;
-            }
+        for (int i = 0; i < word2.length; i++){
+            sb2.append(word2[i]);
         }
-        return k == sb.length() && i == word2.length && j == word2[i-1].length();
+        if (sb1.toString().equals(sb2.toString()))
+            return true;
+        return false;
+        
+        // int k = 0, i = 0, j = 0;
+        // for (i = 0; i < word2.length; i++){
+        //     for (j = 0; j < word2[i].length() && k < sb.length(); j++){
+        //         if (sb.charAt(k) != word2[i].charAt(j))
+        //             return false;
+        //         k++;
+        //     }
+        // }
+        // return k == sb.length() && i == word2.length && j == word2[i-1].length();
         // if (k != sb.length() - 1)
         //     return false;
         // return true;
