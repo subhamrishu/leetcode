650211b8d159b13700a31554bad4742e05269e9f
Subham Kumar <shub.rishu@gmail.com>
2023-12-16 23:06:35 -0800

    Time: 3 ms, Memory: 41.4 MB - LeetHub

--- partition-labels/partition-labels.java
+++ partition-labels/partition-labels.java
@@ -1,21 +1,24 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
         List<Integer> sol = new ArrayList<>();
-        Map<Character, int[]> map = new HashMap<>();
+        // Map<Character, int[]> map = new HashMap<>();
+        int[] lastInd = new int[26];
+        char[] arr=s.toCharArray();
         for (int i = 0; i < s.length(); i++){
-            if (!map.containsKey(s.charAt(i))){
-                map.put(s.charAt(i), new int[2]);
-                map.get(s.charAt(i))[0] = i;
-                // map.get(s.charAt(i))[1] = i;
-            }
-            map.get(s.charAt(i))[1] = i;
+            lastInd[arr[i]-'a'] = i;
+            // if (!map.containsKey(arr[i])){
+            //     map.put(arr[i], new int[2]);
+            //     map.get(arr[i])[0] = i;
+            //     // map.get(s.charAt(i))[1] = i;
+            // }
+            // map.get(arr[i])[1] = i;
         }
         int j, max;
         for (int i = 0; i < s.length(); i++){
             j = i;
-            max = map.get(s.charAt(i))[1];
+            max = lastInd[arr[i]-'a'];//map.get(arr[i])[1];
             while (i < max){
-                max = Math.max(max, map.get(s.charAt(i))[1]);
+                max = Math.max(max, lastInd[arr[i]-'a']);
                 i++;
             }
             sol.add(max-j+1);
