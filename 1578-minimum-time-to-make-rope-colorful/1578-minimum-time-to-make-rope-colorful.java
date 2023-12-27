a441a2ceef87a46314ec5f61857bd0a23c3dff99
Subham Kumar <shub.rishu@gmail.com>
2023-12-27 10:34:04 +0530

    Time: 12 ms (14.91%), Space: 60.7 MB (15.68%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -1,21 +1,27 @@
 class Solution {
     public int minCost(String colors, int[] neededTime) {
-        int minT = 0;
-        for (int i = 1; i < colors.length(); i++){
-            int j = i, maxLocal = neededTime[i-1], sum = neededTime[i-1];
+        int total = 0, currMaxTime= 0;
+        for (int i = 0; i < colors.length(); i++){
+            // int j = i, maxLocal = neededTime[i-1], sum = neededTime[i-1];
             
-            while (j < colors.length() && colors.charAt(j) == colors.charAt(j-1)){
-                maxLocal = Math.max(maxLocal, neededTime[j]);
-                sum += neededTime[j];
-                j++;
+//             while (j < colors.length() && colors.charAt(j) == colors.charAt(j-1)){
+//                 maxLocal = Math.max(maxLocal, neededTime[j]);
+//                 sum += neededTime[j];
+//                 j++;
                 
+//             }
+//             // System.out.println("i = "+ i+" j = "+j+" sum = "+ sum+ " minLocal = "+ maxLocal);
+//             if (j != i){
+//                 minT += (sum - maxLocal);
+//                 i = j;
+//             }
+            
+            if (i > 0 && colors.charAt(i) != colors.charAt(i-1)){
+                currMaxTime = 0;
             }
-            // System.out.println("i = "+ i+" j = "+j+" sum = "+ sum+ " minLocal = "+ maxLocal);
-            if (j != i){
-                minT += (sum - maxLocal);
-                i = j;
-            }
+            total += Math.min(currMaxTime, neededTime[i]);
+            currMaxTime = Math.max(currMaxTime, neededTime[i]);
         }
-        return minT;
+        return total;
     }
 }
