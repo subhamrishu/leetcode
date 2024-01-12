a6f0313a581fe44da631b9b37e6220433a089e05
Subham Kumar <shub.rishu@gmail.com>
2024-01-12 14:45:08 +0530

    Time: 1 ms (100.00%), Space: 41.4 MB (62.42%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.java
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.java
@@ -1,30 +1,30 @@
 class Solution {
+    boolean isVowel(char ch){
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
     public int countVowelSubstrings(String word) {
-        int i = 0, j = 0, sol = 0;
-        Set<Character> set = new HashSet<>();
-        // int[] freq = new int[26];
+        int i =0, j =0, left = 0, distinct = 0, sol = 0;
         char[] cs = word.toCharArray();
-        int  n = word.length();
-       while (i < n){
-           // while (i < n-4 && "aeiou".indexOf(cs[i]) == -1){
-           //     i++;
-           // }
-           j = i;
-           set.clear();
-           // int[] freq = new int[26];
-           while (j < n && "aeiou".indexOf(cs[j]) != -1){
-               // freq[cs[j]-'a']++;
-               set.add(cs[j]);
-               if (set.size() == 5)
-                   sol++;
-               
-               j++;
-           }
-           
-           i++;
-           
-       }
+        int[] freq = new int[26];
+        int n = word.length();
+        for (int right = 0; right < n; right++){
+            if (isVowel(cs[right])){
+                if (++freq[cs[right] - 'a'] == 1){
+                    distinct++;
+                }
+                while (distinct == 5){
+                   if (--freq[cs[i++] - 'a'] == 0){
+                       distinct--;
+                   }
+                    
+                }
+                sol += i - left;
+            }else{
+                i = left = right + 1;
+                distinct = 0;
+                freq = new int[26];
+            }
+        }
         return sol;
-        
     }
 }
