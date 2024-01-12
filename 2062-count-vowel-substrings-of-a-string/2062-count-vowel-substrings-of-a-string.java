f7001c3851564b1d8a1ea89f886c000e11e85816
Subham Kumar <shub.rishu@gmail.com>
2024-01-12 12:07:26 +0530

    Time: 11 ms (57.14%), Space: 41.7 MB (44.18%) - LeetHub

--- 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.java
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.java
@@ -1,19 +1,19 @@
 class Solution {
     public int countVowelSubstrings(String word) {
         int i = 0, j = 0, sol = 0;
-        
+        Set<Character> set = new HashSet<>();
         // int[] freq = new int[26];
         char[] cs = word.toCharArray();
         int  n = word.length();
        while (i < n){
-           while (i < n && "aeiou".indexOf(cs[i]) == -1){
-               i++;
-           }
+           // while (i < n-4 && "aeiou".indexOf(cs[i]) == -1){
+           //     i++;
+           // }
            j = i;
-           Set<Character> set = new HashSet<>();
-           int[] freq = new int[26];
+           set.clear();
+           // int[] freq = new int[26];
            while (j < n && "aeiou".indexOf(cs[j]) != -1){
-               freq[cs[j]-'a']++;
+               // freq[cs[j]-'a']++;
                set.add(cs[j]);
                if (set.size() == 5)
                    sol++;
