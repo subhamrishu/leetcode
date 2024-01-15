17eedeca59d2ee724297decd7525a254e41dfe24
Subham Kumar <shub.rishu@gmail.com>
2024-01-15 11:07:26 +0530

    Time: 319 ms (6.35%), Space: 84.6 MB (91.08%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,34 +1,26 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
-        Set<Integer> sw = new TreeSet<>();
-        Set<Integer> sl = new TreeSet<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < matches.length; i++){
-            sl.add(matches[i][1]);
-            if (sl.contains(matches[i][0])){
-                sw.remove(matches[i][0]);
-            }else{
-                sw.add(matches[i][0]);
-            }
-            if (sw.contains(matches[i][1])){
-                sw.remove(matches[i][1]);
-            }
-            map.put(matches[i][1], map.getOrDefault(matches[i][1], 0)+1);
-        }
-        List<Integer> l = new ArrayList<>();
-        List<Integer> w = new ArrayList<>(sw);
-        
-        for (int k: sl){
-            if(map.get(k) == 1){
-                l.add(k);
-            }
-        }
         List<List<Integer>> sol = new ArrayList<>();
         
+        Map<Integer, Integer> map = new TreeMap<>();
+        
+        for (int i = 0; i < matches.length; i++){
+            map.put(matches[i][0], map.getOrDefault(matches[i][0], 0));
+            map.put(matches[i][1], map.getOrDefault(matches[i][1], 0)+1);
+        }
+        List<Integer> l= new ArrayList<>();
+        List<Integer> w = new ArrayList<>();
+        
+        
+        for (int key: map.keySet()){
+            int val = map.get(key);
+            if (val == 0)
+                w.add(key);
+            else if (val == 1)
+                l.add(key);
+        }
         sol.add(w);
         sol.add(l);
-        
         return sol;
     }
 }
