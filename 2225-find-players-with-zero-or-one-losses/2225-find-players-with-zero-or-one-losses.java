69f1f9e55e00a9765839555cfe64934f07982372
Subham Kumar <shub.rishu@gmail.com>
2024-01-15 11:12:26 +0530

    Time: 30 ms (95.31%), Space: 93.4 MB (57.96%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -2,22 +2,37 @@ class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
         List<List<Integer>> sol = new ArrayList<>();
         
-        Map<Integer, Integer> map = new TreeMap<>();
+        // Map<Integer, Integer> map = new TreeMap<>();
+        int[] freq = new int[100001];
         
         for (int i = 0; i < matches.length; i++){
-            map.put(matches[i][0], map.getOrDefault(matches[i][0], 0));
-            map.put(matches[i][1], map.getOrDefault(matches[i][1], 0)+1);
+            int win = matches[i][0];
+            int loss = matches[i][1];
+            // map.put(matches[i][0], map.getOrDefault(matches[i][0], 0));
+            // map.put(matches[i][1], map.getOrDefault(matches[i][1], 0)+1);
+            if (freq[win] == 0)
+                freq[win] = -1;
+            if (freq[loss] == -1)
+                freq[loss] = 1;
+            else
+                freq[loss]++;
         }
         List<Integer> l= new ArrayList<>();
         List<Integer> w = new ArrayList<>();
         
         
-        for (int key: map.keySet()){
-            int val = map.get(key);
-            if (val == 0)
-                w.add(key);
-            else if (val == 1)
-                l.add(key);
+        // for (int key: map.keySet()){
+        //     int val = map.get(key);
+        //     if (val == 0)
+        //         w.add(key);
+        //     else if (val == 1)
+        //         l.add(key);
+        // }
+        for (int i = 1; i < freq.length; i++){
+            if (freq[i] == -1)
+                w.add(i);
+            if (freq[i] == 1)
+                l.add(i);
         }
         sol.add(w);
         sol.add(l);
