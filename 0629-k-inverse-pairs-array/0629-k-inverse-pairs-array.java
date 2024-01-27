39e7a8f0a814c64c593f9d551725141f505dcb8a
Subham Kumar <shub.rishu@gmail.com>
2024-01-27 07:56:10 -0800

    Time: 1817 ms (9.25%), Space: 70.4 MB (12.61%) - LeetHub

---
+++ 0629-k-inverse-pairs-array/0629-k-inverse-pairs-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    Integer[][] memo = new Integer[1001][1001];
+    public int kInversePairs(int n, int k) {
+        if (n == 0)
+            return 0;
+        if (k == 0)
+            return 1;
+        if (memo[n][k] != null)
+            return memo[n][k];
+        int inv = 0;
+        for (int i = 0; i <= Math.min(k, n - 1); i++)
+            inv = (inv + kInversePairs(n - 1, k - i)) % 1000000007;
+        return memo[n][k] = inv;
+    }
+}
