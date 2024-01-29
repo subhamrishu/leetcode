e1cbb933cd23711e1881e5b8b1beb9efc8f9af05
Subham Kumar <shub.rishu@gmail.com>
2024-01-29 09:35:19 -0800

    Time: 0 ms (100.00%), Space: 41.8 MB (5.57%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -12,40 +12,26 @@ class MyQueue {
     
     public void push(int x) {
         total++;
-        // System.out.println("Before push = "+x+" stack 1 = "+s1+" stack 2 = "+s2);
         s1.push(x);
-        // System.out.println("\tAfter push = "+x+" stack 1 = "+s1+" stack 2 = "+s2);
-        // if (s2.isEmpty())
-        //     s2.push(x);
     }
     
     public int pop() {
-        // System.out.println("Before pop = " + " stack 1 = "+s1+" stack 2 = "+s2 +" poppedNum = "+poppedNum);
         if (!s2.isEmpty()){
-            // poppedNum++;
             total--;
             int top = s2.pop();
-            // System.out.println("\tAfter pop = " + " stack 1 = "+s1+" stack 2 = "+s2);
             return top;
         }
         while (!s1.isEmpty()){
             s2.push(s1.pop());
         }
-        // while (poppedNum > 0){
-        //     poppedNum--;
-        //     s2.pop();
-        // }
-        // System.out.println("\tAfter pop = " + " stack 1 = "+s1+" stack 2 = "+s2);
         total--;
         return s2.pop();
     }
     
     public int peek() {
-        // System.out.print("peek: ");
         int top = pop();
         s2.push(top);
         total++;
-        // poppedNum--;
         return top;
     }
     
