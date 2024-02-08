62241b38523f9c206245e3e5bd193502c9026618
Subham Kumar <shub.rishu@gmail.com>
2024-02-08 10:22:39 -0800

    Time: 29 ms (81.27%), Space: 44.8 MB (18.89%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int numSquares(int n) {
+    int helper1(int n){
         int[] dp = new int[n+1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
@@ -19,5 +19,33 @@ class Solution {
             }
         }
         return dp[n];
+    }
+    public int numSquares(int n) {
+       // return helper1(n);
+        int max_squares_num = (int)Math.sqrt(n)+1;
+        int[] squares = new int[max_squares_num];
+        
+        for (int i = 1; i < max_squares_num; i++){
+            squares[i] = i * i;
+        }
+        Set<Integer> q = new HashSet<>();
+        q.add(n);
+        int level = 0;
+        
+        while (!q.isEmpty()){
+            level += 1;
+            Set<Integer> q_temp = new HashSet<>();
+            for (Integer remainder: q){
+                for (int i = 1; i < max_squares_num; i++){
+                    if (remainder < squares[i])
+                        break;
+                    else if (remainder == squares[i])
+                        return level;
+                    q_temp.add(remainder - squares[i]);
+                }
+            }
+            q = q_temp;
+        }
+        return level;
     }
 }
