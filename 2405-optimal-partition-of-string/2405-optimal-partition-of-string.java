c39fda71c9a1fa744ce68b6652d29a59c5d60ab8
Subham Kumar <shub.rishu@gmail.com>
2024-02-12 10:39:13 -0800

    Time: 8 ms (88.81%), Space: 45.8 MB (16.32%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -1,17 +1,14 @@
 class Solution {
     public int partitionString(String s) {
-        int sol = 0, n = s.length();
+        int sol = 0;
         int[] freq = new int[26];
-        for (int i = 0; i < n; i++){
-            int j = i;
-            freq = new int[26];
-            while (j < n && freq[s.charAt(j)-'a'] == 0){
-                freq[s.charAt(j)-'a']++;
-                j++;
+        for (char ch: s.toCharArray()){
+            if (freq[ch-'a'] == 1){
+                sol++;
+                freq = new int[26];
             }
-            i = j - 1;
-            sol++;
+            freq[ch-'a'] = 1;
         }
-        return sol;
+        return sol+1;
     }
 }
