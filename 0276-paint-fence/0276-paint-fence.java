e30a25aa50efc548304b7208a5cd2bbf48000a3d
Subham Kumar <shub.rishu@gmail.com>
2024-02-14 08:32:43 -0800

    Time: 0 ms (100.00%), Space: 40.3 MB (58.82%) - LeetHub

--- 0276-paint-fence/0276-paint-fence.java
+++ 0276-paint-fence/0276-paint-fence.java
@@ -14,6 +14,18 @@ class Solution {
     }
     
     public int numWays(int n, int k) {
-        return totalWays(n, k);
+        if (n == 1) return k;
+        if (n == 2) return k * k;
+        
+        int totalWays[] = new int[n + 1];
+        totalWays[1] = k;
+        totalWays[2] = k * k;
+        
+        for (int i = 3; i <= n; i++) {
+            totalWays[i] = (k - 1) * (totalWays[i - 1] + totalWays[i - 2]);
+        }
+        
+        return totalWays[n];
+        // return totalWays(n, k);
     }
 }
