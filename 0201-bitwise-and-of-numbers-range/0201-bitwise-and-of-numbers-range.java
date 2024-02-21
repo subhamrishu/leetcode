309d3679d08dc5b36dd1bf664b1b0158d6b97be2
Subham Kumar <shub.rishu@gmail.com>
2024-02-21 10:20:21 -0800

    Time: 3 ms (100.00%), Space: 43.7 MB (68.77%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
@@ -1,35 +1,44 @@
 class Solution {
-    public int rangeBitwiseAnd(int left, int right) {
-        if (left == 0 || right == 0)
-            return 0;
-        if (left == right)
-            return left;
-        long p = 0;
-        for (int i = 0; i <= 32; i++){
-            p = (long)Math.pow(2, i);
-            // if (p >= left && p <= right){
-            //     break;            
-            // }
-            if (p > (long)right){
-                p = (long)Math.pow(2, i-1);
-                break;
+//     public int rangeBitwiseAnd(int left, int right) {
+//         if (left == 0 || right == 0)
+//             return 0;
+//         if (left == right)
+//             return left;
+//         long p = 0;
+//         for (int i = 0; i <= 32; i++){
+//             p = (long)Math.pow(2, i);
+//             // if (p >= left && p <= right){
+//             //     break;            
+//             // }
+//             if (p > (long)right){
+//                 p = (long)Math.pow(2, i-1);
+//                 break;
+//             }
+//         }
+        
+//         if (p != 0 && p <= right && p > left){
+//             // if (p < right)
+//             //     return 0;
+//             // left = p;
+//             return 0;
+//         }
+        
+        
+//         // left = left < (int)p ? (int)p : left;
+        
+//         // System.out.println("p = "+p+" left = "+left);
+//         for (int i = left; i <= right && i>=0; i++){
+//             left &= i;
+//         }
+//         return left;
+//     }
+        public int rangeBitwiseAnd(int left, int right) {
+            int res = right;
+            while(right > left) {
+                res = right & (right-1);
+                right = res;
             }
-        }
+            return res;
         
-        if (p != 0 && p <= right && p > left){
-            // if (p < right)
-            //     return 0;
-            // left = p;
-            return 0;
-        }
-        
-        
-        // left = left < (int)p ? (int)p : left;
-        
-        // System.out.println("p = "+p+" left = "+left);
-        for (int i = left; i <= right && i>=0; i++){
-            left &= i;
-        }
-        return left;
     }
 }
