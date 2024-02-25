83bb5a1652f07266a9d759e6d66b602b4a75f324
Subham Kumar <shub.rishu@gmail.com>
2024-02-25 08:30:49 -0800

    Time: 327 ms (43.64%), Space: 90.5 MB (5.45%) - LeetHub

---
+++ 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.java
@@ -0,0 +1,53 @@
+class Solution {
+    Map<Integer, List<Integer>> prime2index = new HashMap<>();
+    Map<Integer, List<Integer>> index2prime = new HashMap<>();
+    
+    public void dfs(int index, boolean[] visitedIndex, Map<Integer, Boolean> visitedPrime) {
+
+        if (visitedIndex[index])
+            return;
+        visitedIndex[index] = true;
+
+        for (int prime : index2prime.get(index)) {
+            if (visitedPrime.getOrDefault(prime, false))
+                continue;
+            visitedPrime.put(prime, true);
+            for (int index1 : prime2index.get(prime)) {
+                if (visitedIndex[index1])
+                    continue;
+                dfs(index1, visitedIndex, visitedPrime);
+            }
+        }
+    }
+
+    public boolean canTraverseAllPairs(int[] nums) {
+        if (nums.length == 1)
+            return true;
+        for (int i = 0; i < nums.length; i++) {
+            int temp = nums[i];
+            if (temp == 1)
+                return false;
+            for (int j = 2; j * j <= nums[i]; j++) {
+                if (temp % j == 0) {
+                    prime2index.computeIfAbsent(j, k -> new ArrayList<>()).add(i);
+                    index2prime.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
+                    while (temp % j == 0)
+                        temp /= j;
+                }
+            }
+            if (temp > 1) {
+                prime2index.computeIfAbsent(temp, k -> new ArrayList<>()).add(i);
+                index2prime.computeIfAbsent(i, k -> new ArrayList<>()).add(temp);
+            }
+        }
+
+        boolean[] visitedIndex = new boolean[nums.length];
+        Map<Integer, Boolean> visitedPrime = new HashMap<>();
+        dfs(0, visitedIndex, visitedPrime);
+
+        for (boolean b : visitedIndex)
+            if (!b)
+                return false;
+        return true;
+    }
+}
