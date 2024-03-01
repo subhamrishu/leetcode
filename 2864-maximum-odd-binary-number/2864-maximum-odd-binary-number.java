63d2b143d48eebc5afb9eb11bc3cdd16cff3d11a
Subham Kumar <shub.rishu@gmail.com>
2024-03-01 12:01:47 -0800

    Time: 2 ms (83.02%), Space: 43.3 MB (58.56%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.java
@@ -6,14 +6,17 @@ class Solution {
             if (ch == '1')
                 count++;
         }
-        sb.append('1');
-        count--;
-        for (int i = 0; i < n-count-1; i++){
-            sb.append('0');
+        // sb.append('1');
+        // count--;
+        for (int i = 0; i < n-1; i++, count--){
+            if (count > 1)
+                sb.append('1');
+            else
+                sb.append('0');
         }
-        for (int i = 0; i < count; i++){
-            sb.append('1');
-        }
-        return sb.reverse().toString();
+        // for (int i = 0; i < count; i++){
+        //     sb.append('1');
+        // }
+        return sb.append('1').toString();
     }
 }
