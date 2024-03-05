1ae253c035d7df9a0625f5db3bea35c309c9a8bf
Subham Kumar <shub.rishu@gmail.com>
2024-03-05 09:57:25 -0800

    Time: 3 ms (100.00%), Space: 45.2 MB (40.95%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
@@ -6,13 +6,13 @@ class Solution {
         
         while (i < j && cs[i] == cs[j]){
             char currChar = cs[i];
-            while (i < j && cs[i] == currChar)
+            while (i <= j && cs[i] == currChar)
                 i++;
-            if (i == j) return 0;
-            while (i < j && currChar == cs[j])
+            // if (i == j) return 0;
+            while (i <= j && currChar == cs[j])
                 j--;
             // System.out.println("i="+i+" j="+j);
-            if (i == j) return j-i+1;
+            // if (i == j) return j-i+1;
         }
         // System.out.println("i="+i+" j="+j);
         return j-i+1;
