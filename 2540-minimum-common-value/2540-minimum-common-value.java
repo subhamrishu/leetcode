4e8e190b156b64feb1ca9d05b68ec749720a5c42
Subham Kumar <shub.rishu@gmail.com>
2024-03-08 16:59:46 -0800

    Time: 2 ms (81.56%), Space: 62.2 MB (92.22%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,7 +1,7 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        // Arrays.sort(nums1);
+        // Arrays.sort(nums2);
         
         int common = -1;
         for (int i = 0, j = 0; i < nums1.length && j < nums2.length;){
