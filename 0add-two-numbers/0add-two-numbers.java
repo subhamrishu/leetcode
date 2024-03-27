7aa33b0558861789db6d1b920bbc306ba8037cf5
Subham Kumar <shub.rishu@gmail.com>
2024-03-27 16:30:35 -0700

    Time: 1 ms, Memory: 44.1 MB - LeetHub

--- 0add-two-numbers/0add-two-numbers.java
+++ 0add-two-numbers/0add-two-numbers.java
@@ -14,33 +14,22 @@ class Solution {
         ListNode dummy = new ListNode();
         ListNode newHead = dummy;
         
-        while (l1 != null && l2 != null){
-            int sum = l1.val + l2.val + carry;
+        while (l1 != null || l2 != null || carry != 0){
+            int sum = 0;
+            if (l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            sum += carry;
             newHead.next = new ListNode(sum%10);
             newHead = newHead.next;
             carry = sum / 10;
-            l1 = l1.next;
-            l2 = l2.next;
         }
-        while (l1 != null){
-            int sum = l1.val + carry;
-            newHead.next = new ListNode(sum%10);
-            newHead = newHead.next;
-            carry = sum / 10;
-            l1 = l1.next;
-        }
-        while (l2 != null){
-            int sum = l2.val + carry;
-            newHead.next = new ListNode(sum%10);
-            newHead = newHead.next;
-            carry = sum / 10;
-            l2 = l2.next;
-        }
-        
-        if (carry != 0){
-            newHead.next = new ListNode(carry);
-            newHead = newHead.next;
-        }
+       
         newHead.next = null;
         return dummy.next;
     }
